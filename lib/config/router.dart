5eb3a317606fb47dfbcb384c482c1f0c56b09cc2
Lucas GUILLEMINOT <lcs.glmt@gmail.com>
2022-07-09 21:22:13 +0200

    Initial commit

---
+++ lib/config/router.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/cupertino.dart';
+import 'package:portfolio/pages/home_page.dart';
+
+/// Routeur
+
+final routes = <String, WidgetBuilder> {
+  "/": (builder) => const HomePage()
+};
+
+const pathHomePage = "/";
